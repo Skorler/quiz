229538efc2a52ea46f58bb1c71617587ecf7f7ef
Alesia_Sergeeva <alesiasiarheeva@gmail.com>
2020-08-26 14:48:34 +0300

    Add RatingController and template for it

---
+++ src/Controller/RatingController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RatingController extends AbstractController
+{
+    /**
+     * @Route("/rating", name="rating")
+     */
+    public function showRating()
+    {
+        return $this->render('rating/showRating.html.twig', [
+            'controller_name' => 'RatingController',
+        ]);
+    }
+}
