1920e245e70a6427ce5e4a9761a9710a74e7242c
Alesia_Sergeeva <alesiasiarheeva@gmail.com>
2020-08-26 21:45:27 +0300

    Change templates

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuestionController extends AbstractController
+{
+    /**
+     * @Route("/question/{slug}", name="question")
+     */
+    public function showQuestion($slug) : Response
+    {
+        return $this->render('question/show.html.twig', [
+            'controller_name' => 'QuestionController',
+        ]);
+    }
+}
